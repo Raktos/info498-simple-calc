6a6bc59f8f76d80d3c80a4cb8014ec4f98758ffb
Jason Ho <raktos@uw.edu>
2015-10-08 16:59:27 -0700

    everything but basic math working

--- simplecalc/simplecalc/main.swift
+++ simplecalc/simplecalc/main.swift
@@ -15,14 +15,74 @@ func input() -> String {
     return result.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
 }
 
-func addInput() -> Bool {
+class Calculator {
+    var inputs : [String]
     
+    init() {
+        inputs = [String]()
+    }
+    
+    private func convert(incoming:String) -> Double {
+        return NSNumberFormatter().numberFromString(incoming)!.doubleValue
+    }
+    
+    func getInput(incoming:String) {
+        inputs.append(incoming)
+    }
+    
+    func add() -> Double {
+        return convert(inputs[0]) + convert(inputs[2])
+    }
+    
+    func sub() -> Double {
+        return convert(inputs[0]) - convert(inputs[2])
+    }
+    
+    func mul() -> Double {
+        return convert(inputs[0]) * convert(inputs[2])
+    }
+    
+    func div() -> Double {
+        return convert(inputs[0]) / convert(inputs[2])
+    }
+    
+    func mod() -> Double {
+        return convert(inputs[0]) % convert(inputs[2])
+    }
+    
+    func avg() -> Double {
+        var total = 0.0
+        for number in inputs {
+            total += convert(number)
+        }
+        
+        return total / Double(numInputs())
+    }
+    
+    func fact() -> Double {
+        var total = convert(inputs[0])
+        
+        for var i = convert(inputs[0]) - 1; i > 1; i-- {
+            total *= i
+        }
+        
+        return total
+    }
+    
+    func count() -> Double {
+        return Double(numInputs())
+    }
+
+    private func numInputs() -> Int {
+        return inputs.count
+    }
 }
 
 while true { //run loop
     var gatheringInput = true
     var basicMathFlag = false
     var inputNum = 0
+    let calc = Calculator()
     println("Enter an expression separated by returns:")
     while gatheringInput {
         gatheringInput = false
@@ -31,23 +91,28 @@ while true { //run loop
         case "help":
             println("THIS IS THE HELP MENU")
         case "count":
-            println("COUNT")
+            println("\(calc.count())")
         case "avg":
-            println("AVERAGE")
+            println("\(calc.avg())")
         case "fact":
-            println("FACTORIAL")
-        case where basicMathFlag:
-            println("DO BASIC MATHS")
+            if calc.numInputs() > 1{
+                println("cannot factorial more than one input")
+            } else {
+                println("\(calc.fact())")
+            }
         default:
+            calc.getInput(thisInput)
             gatheringInput = true
-            println("ADD INPUT")
         }
     }
 }
 
-class InputList {
-    var inputs : [String]
-    init() {
-        
-    }
-}
+/*switch thisInput{
+case "+", "add":
+case "-", "sub":
+case "*", "mul":
+case "/", "div":
+case "%", "mod":
+default:
+}*/
+
