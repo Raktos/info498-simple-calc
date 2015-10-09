9442379f62bf4edd97f8bf0186e467969d91e688
Jason Ho <raktos@uw.edu>
2015-10-08 19:27:39 -0700

    comments, etc.

--- simplecalc/simplecalc/main.swift
+++ simplecalc/simplecalc/main.swift
@@ -2,12 +2,13 @@
 //  main.swift
 //  simplecalc
 //
-//  Created by iGuest on 10/7/15.
+//  Created by Jason Ho on 10/7/15.
 //  Copyright (c) 2015 Jason Ho. All rights reserved.
 //
 
 import Foundation
 
+//gets input from console
 func input() -> String {
     let keyboard = NSFileHandle.fileHandleWithStandardInput()
     let inputData = keyboard.availableData
@@ -15,41 +16,52 @@ func input() -> String {
     return result.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
 }
 
+//does calculations, stores inputs
 class Calculator {
+    //stores inputs
+    //TODO could be an array of Doubles, does store commands anymore
     var inputs : [String]
     
     init() {
         inputs = [String]()
     }
     
+    //converts strings to Doubles
     private func convert(incoming : String) -> Double {
         return NSNumberFormatter().numberFromString(incoming)!.doubleValue
     }
     
+    //stores an input
     func getInput(incoming:String) {
         inputs.append(incoming)
     }
     
+    //addition
     func add() -> Double {
         return convert(inputs[0]) + convert(inputs[1])
     }
     
+    //subtraction
     func sub() -> Double {
         return convert(inputs[0]) - convert(inputs[1])
     }
     
+    //multiplication
     func mul() -> Double {
         return convert(inputs[0]) * convert(inputs[1])
     }
     
+    //division
     func div() -> Double {
         return convert(inputs[0]) / convert(inputs[1])
     }
     
+    //mod
     func mod() -> Double {
         return convert(inputs[0]) % convert(inputs[1])
     }
     
+    //averages stored inputs
     func avg() -> Double {
         var total = 0.0
         for number in inputs {
@@ -59,6 +71,7 @@ class Calculator {
         return total / Double(numInputs())
     }
     
+    //factorial of first input
     func fact() -> Double {
         var total = convert(inputs[0])
         
@@ -68,26 +81,35 @@ class Calculator {
         
         return total
     }
-    
+     //counts total inputs
     func count() -> Double {
         return Double(numInputs())
     }
-
+    
+    //private, counts total input
     private func numInputs() -> Int {
         return inputs.count
     }
 }
 
 var running = true
-while running { //run loop
+
+//main loop
+while running {
     var gatheringInput = true
     var basicMathFlag = false
     var inputNum = 0
     let calc = Calculator()
+    
+    //starts checking for input
     println("Enter an expression separated by returns:")
+    
+    //gather input loops, will go around again after an operation is finished (or fails)
     while gatheringInput {
         gatheringInput = false
         let thisInput = input()
+        
+        //handle inputs
         switch thisInput {
         case "quit", "q":
             running = false
@@ -103,6 +125,8 @@ while running { //run loop
             } else {
                 println("\(calc.fact())")
             }
+        
+        //handle basic math functions
         case"+", "add", "-", "sub", "*", "mul", "/", "div", "%", "mod":
             if calc.count() > 1 {
                 println("too many operands")
@@ -120,22 +144,14 @@ while running { //run loop
                 case "%", "mod":
                     println("\(calc.mod())")
                 default:
-                    println("UNKNOWN ERROR")
+                    break
                 }
             }
+        
+        //default, put input into calculator
         default:
             calc.getInput(thisInput)
             gatheringInput = true
         }
     }
 }
-
-/*switch thisInput{
-case "+", "add":
-case "-", "sub":
-case "*", "mul":
-case "/", "div":
-case "%", "mod":
-default:
-}*/
-
